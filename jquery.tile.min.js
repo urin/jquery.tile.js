03d998887c40cf0a24ac62aaae73d0a9c2b69678
urin <urinkun@gmail.com>
2013-01-25 10:16:42 +0900

    Add minified version.

---
+++ jquery.tile.min.js
@@ -0,0 +1,1 @@
+(function(a){a.fn.tile=function(b){var c,d,e,f,g=this.length-1,h;if(!b)b=this.length;this.each(function(){h=this.style;if(h.removeProperty)h.removeProperty("height");else if(h.removeAttribute)h.removeAttribute("height")});return this.each(function(h){e=h%b;if(e==0)c=[];c[e]=a(this);f=c[e].height();if(e==0||f>d)d=f;if(h==g||e==b-1)a.each(c,function(){this.height(d)})})}})(jQuery)
