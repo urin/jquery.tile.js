41fc147b3e8aa22a192c28eed25a1db4f50aadbf
urin <urinkun@gmail.com>
2013-01-27 22:17:18 +0900

    Modify links in js comment.

--- jquery.tile.js
+++ jquery.tile.js
@@ -5,7 +5,7 @@
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
- * @author: Hayato Takenaka (http://urin.take-uma.net)
+ * @author: Hayato Takenaka (https://github.com/urin/jquery.tile.js)
  * @version: 0.0.3
 **/
 ;(function($) {
