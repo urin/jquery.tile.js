af909df31248ed5873bd46425334534e2b016459
urin <urinkun@gmail.com>
2014-02-27 21:25:32 +0530

    Burshed up.

--- js/jquery.tile.js
+++ js/jquery.tile.js
@@ -6,25 +6,27 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (https://github.com/urin/jquery.tile.js)
- * @version: 1.0.1
+ * @version: 1.0.2
  **/
 (function($) {
   $.fn.tile = function(columns) {
-    var tiles, $tile, max, c, h, a = ["height"], last = this.length - 1, s, method;
+    var tiles, $tile, max, c, h, s, a = ["height"],
+      last = this.length - 1, remove, height;
     if(!columns) columns = this.length;
     s = document.body.style;
-    method = s.removeProperty ? s.removeProperty : s.removeAttribute;
-    this.each(function() { method.apply(this.style, a); });
-    return this.each(function(i) {
+    remove = s.removeProperty ? s.removeProperty : s.removeAttribute;
+    return this.each(function() {
+      remove.apply(this.style, a);
+    }).each(function(i) {
       c = i % columns;
       if(c == 0) tiles = [];
       $tile = tiles[c] = $(this);
-      method = ($tile.css("box-sizing") == "border-box") ? $.fn.outerHeight : $.fn.innerHeight;
-      h = method.apply($tile);
+      height = ($tile.css("box-sizing") == "border-box") ? $.fn.outerHeight : $.fn.innerHeight;
+      h = height.apply($tile);
       if(c == 0 || h > max) max = h;
       if(i == last || c == columns - 1) {
         max = [max];
-        $.each(tiles, function() { method.apply(this, max); });
+        $.each(tiles, function() { height.apply(this, max); });
       }
     });
   };
