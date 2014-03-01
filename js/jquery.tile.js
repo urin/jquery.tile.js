ecfa27c998fc7806a901000ec3cb6e0b5f01f2d8
urin <urinkun@gmail.com>
2014-03-01 22:48:14 +0530

    Updated to 1.1.0.

--- js/jquery.tile.js
+++ js/jquery.tile.js
@@ -6,12 +6,12 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (https://github.com/urin/jquery.tile.js)
- * @version: 1.0.2
+ * @version: 1.1.0
  **/
 (function($) {
   $.fn.tile = function(columns) {
-    var tiles, $tile, max, c, h, s, a = ["height"],
-      last = this.length - 1, remove, height;
+    var tiles, $tile, max, c, h, s, remove, a = ["height"],
+      last = this.length - 1;
     if(!columns) columns = this.length;
     s = document.body.style;
     remove = s.removeProperty ? s.removeProperty : s.removeAttribute;
@@ -21,14 +21,11 @@
       c = i % columns;
       if(c == 0) tiles = [];
       $tile = tiles[c] = $(this);
-      height = ($tile.css("box-sizing") == "border-box") ? $.fn.outerHeight : $.fn.innerHeight;
-      h = height.apply($tile);
+      h = ($tile.css("box-sizing") == "border-box") ? $tile.outerHeight() : $tile.innerHeight();
       if(c == 0 || h > max) max = h;
       if(i == last || c == columns - 1) {
-        max = [max];
-        $.each(tiles, function() { height.apply(this, max); });
+        $.each(tiles, function() { this.css("height", max); });
       }
     });
   };
 })(jQuery);
-
