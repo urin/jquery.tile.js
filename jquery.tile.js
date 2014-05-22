eba6ff0f1ec6f7480ce603f4e8b759dd04cc725b
urin <urinkun@gmail.com>
2014-05-22 19:23:51 +0530

    Modified image url.

--- jquery.tile.js
+++ jquery.tile.js
@@ -6,14 +6,13 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * @author: Hayato Takenaka (https://github.com/urin/jquery.tile.js)
- * @version: 1.1.0
+ * @version: 1.1.1
  **/
 (function($) {
   $.fn.tile = function(columns) {
-    var tiles, $tile, max, c, h, s, remove, a = ["height"],
+    var tiles, $tile, max, c, h, remove, s = document.body.style, a = ["height"],
       last = this.length - 1;
     if(!columns) columns = this.length;
-    s = document.body.style;
     remove = s.removeProperty ? s.removeProperty : s.removeAttribute;
     return this.each(function() {
       remove.apply(this.style, a);
